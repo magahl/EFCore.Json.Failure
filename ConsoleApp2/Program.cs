8470b12430f8a02c5415fe574a970ec9ac91aa46
Magnus Ahlberg <magnus.ahlberg@swegon.com>
2024-03-20 12:00:25 +0100

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ConsoleApp2;
+using Microsoft.EntityFrameworkCore;
+
+var context = new TestContext();
+await Seed(context);
+var persons = await context.Persons.AsNoTracking().Select(x => new Person
+{
+    FirstName = x.FirstName,
+    Id = x.Id,
+    Addresses = x.Addresses.Select(a => new Address
+    {
+        Street = a.Street,
+        Zip = a.Zip
+    }).ToList()
+}).ToListAsync();
+
+Console.WriteLine(persons.Count);
+
+async Task Seed(TestContext testContext)
+{
+    await testContext.Persons.AddAsync(new Person()
+    {
+        FirstName = "Magnus",
+        Id = Guid.NewGuid(),
+        Addresses =
+        [
+            new Address()
+            {
+                Street = "Street",
+                Zip = "55555"
+            },
+            new Address()
+            {
+                Street = "Street",
+                Zip = "44444"
+            }
+        ]
+    });
+
+    await context.SaveChangesAsync();
+}
---
+++ ConsoleApp2/TestContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp2;
+
+public class TestContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.LogTo(Console.WriteLine).EnableSensitiveDataLogging();
+        optionsBuilder.UseSqlServer("Server=(local);Database=Json.Failure;Trusted_Connection=True;MultipleActiveResultSets=True;Integrated Security=True;TrustServerCertificate=true",
+            o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery));
+        optionsBuilder.UseLazyLoadingProxies();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new PersonConfig());
+    }
+}
+
+public class PersonConfig : IEntityTypeConfiguration<Person>
+{
+    public void Configure(EntityTypeBuilder<Person> builder)
+    {
+        builder.ToTable("Persons");
+        builder.HasKey(x => x.Id);
+        builder.OwnsMany(x => x.Addresses, navigationBuilder =>
+        {
+            navigationBuilder.ToJson();
+        });
+    }
+}
+
+public class Person
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public virtual List<Address> Addresses { get; set; }
+}
+
+public class Address
+{
+    public string Street { get; set; }
+    public string Zip { get; set; }
+}
